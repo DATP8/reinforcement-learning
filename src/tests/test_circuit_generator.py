c1d67a16b69b3bb8a6ec4fc2e026278dca563d24
Mikkel Tygesen <mikkel.tygesen@gmail.com>
2026-03-19 15:30:48 +0100

    test for generate radom circuit

---
+++ src/tests/test_circuit_generator.py
@@ -0,0 +1,15 @@
+import unittest
+from circuit_generator import CircuitGenerator
+
+NUM_QUBITS = 4
+NUM_GATES = 10
+GATESET = ["cx", "h", "s"]
+
+class TestCircuitGenerator(unittest.TestCase):
+    def test_generate_random_circuit(self):
+        rand_circuit = CircuitGenerator.generate_random_circuit(NUM_QUBITS, NUM_GATES, ["cx", "h", "s"])
+        self.assertEqual(NUM_QUBITS, rand_circuit.num_qubits)
+        #TODO: add randomized gateset test
+            
+        
+            
