d20af58f3f3dbd0db9f3cea949585189d064dae5
MadsVind <73589644+MadsVind@users.noreply.github.com>
2026-03-30 10:28:52 +0000

    style: auto-format code

--- tests/test_bwas_router.py
+++ tests/test_bwas_router.py
@@ -46,8 +46,13 @@ class TestBWAS(unittest.TestCase):
             )
             routed_dag = circuit_to_dag(routed_circuit)
             post_operator = Operator.from_circuit(
-                routed_circuit, 
-                final_layout= Layout({routed_dag.qubits[org_q]: final_q for org_q, final_q in enumerate(final)})
+                routed_circuit,
+                final_layout=Layout(
+                    {
+                        routed_dag.qubits[org_q]: final_q
+                        for org_q, final_q in enumerate(final)
+                    }
+                ),
             )
             circuit_graph = CircuitGraph.from_circuit(routed_circuit)
             if not state_handler.is_terminal(circuit_graph):
