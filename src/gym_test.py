b15ed61c5d2839b9e352b8c66afb366478559a42
Asbjørn Rysgaard Eriksen <mail@bliztle.com>
2026-04-28 12:22:06 +0200

    feat: use feature extractors

--- src/gym_test.py
+++ src/gym_test.py
@@ -6,7 +6,6 @@ from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.monitor import Monitor
 
 from src.curriculum_callback import CurriculumCallback
-from src.gym_extractor import SimpleExtractor
 from src.policy_types import ActorCriticPolicyType
 from src.ppo_util import PostCurriculumEvalCallback, make_env, mask_fn
 
@@ -51,22 +50,6 @@ if __name__ == "__main__":
         n_envs=n_envs,
     )
 
-    # Simple Extractor
-    policy_kwargs: dict[
-        str, type[SimpleExtractor] | dict[str, int] | dict[str, list[int]]
-    ] = dict(
-        features_extractor_class=SimpleExtractor,
-        features_extractor_kwargs=dict(features_dim=128),
-        net_arch=dict(pi=[64, 64], vf=[64, 64]),
-    )
-
-    # Hybrid Graph Extractor
-    # policy_kwargs = dict(
-    #    features_extractor_class=HybridExtractor,
-    #    features_extractor_kwargs=dict(features_dim=128),
-    #    net_arch=dict(pi=[64, 64], vf=[64, 64]),
-    # )
-
     model = MaskablePPO(
         POLICY_TYPE.get_sb3_policy(),
         train_env,
@@ -74,10 +57,8 @@ if __name__ == "__main__":
         batch_size=BATCH_SIZE,
         n_steps=N_STEPS,
         n_epochs=EPOCHS,
+        policy_kwargs=POLICY_TYPE.get_policy_kwargs()
     )
-    # model = MaskablePPO(
-    #    "MultiInputPolicy", train_env, policy_kwargs=policy_kwargs, verbose=1
-    # )
 
     eval_env = make_env(
         coupling_map=coupling_map,
--- src/policy_types.py
+++ src/policy_types.py
@@ -1,14 +1,34 @@
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+from gym_extractor import HybridExtractor
+from gym_extractor import SimpleExtractor
 from enum import Enum, auto
 
 
 class ActorCriticPolicyType(Enum):
     BASIC = auto()
+    SIMPLE_MLP = auto()
     SIMPLE_GNN = auto()
     HYBRID_GNN = auto()
 
     def get_sb3_policy(self) -> str:
         match self.name:
-            case self.BASIC.name:
+            case self.BASIC.name | self.SIMPLE_MLP.name:
                 return "MlpPolicy"
             case _:
                 return "MultiInputPolicy"
+
+    def get_feature_extractor(self):
+        match self.name:
+            case self.SIMPLE_MLP.name:
+                return SimpleExtractor
+            case self.HYBRID_GNN.name: 
+                return HybridExtractor
+
+    def get_policy_kwargs(self):
+        if self.name == self.BASIC.name:
+            return None
+        return dict(
+                features_extractor_class=self.get_feature_extractor(),
+                features_extractor_kwargs=dict(features_dim=128),
+                net_arch=dict(pi=[64, 64], vf=[64, 64]),
+            )
